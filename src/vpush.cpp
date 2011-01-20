bf8542c32cabd9fac358499104d271fdd2fe91d0
Vivek Sekhar <vivek@xmain.com>
2011-01-20 15:36:56 -0500

    Untested: Direct access to push() and pop()

--- src/vpush.cpp
+++ src/vpush.cpp
@@ -16,13 +16,14 @@ int my_func(int i, int j) { return i+j; }
 badtype badfunc() { return badtype(1); }
 
 int main(int argc, char** argv) {
-	using vpush::stack;
+	using vpush::push;
+	using vpush::pop;
 	using vpush::stacks;
 	vpush::register_(my_func, "my_func");
 	vpush::register_(badfunc, "bad_func");
 	std::cout << "Stacks count: " << stacks().size() << std::endl;
-	stack<int>().push(5);
-	std::cout << "Int stack: " << stack<int>().pop() << std::endl;
+	push(5);
+	std::cout << "Int stack: " << pop<int>() << std::endl;
 	return 0;
 }
 
