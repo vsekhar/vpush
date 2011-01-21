eb0079b65c8043c408fb75b6d9dea467b37508c8
Vivek Sekhar <vivek@xmain.com>
2011-01-21 01:52:23 -0500

    Cleanup and comments

--- include/vpush/stacks.hpp
+++ include/vpush/stacks.hpp
@@ -2,15 +2,16 @@
 #define __VPUSH_STACKS_HPP__
 
 #include <map>
-#include <boost/ref.hpp>
-#include <vpush/util/singleton.hpp>
 #include <vpush/stack_fwd.hpp>
+#include <vpush/util/typeinfo.hpp>
+#include <vpush/util/singleton.hpp>
 
 namespace vpush {
 
 typedef std::map<util::TypeInfo, detail::stack_base*> stacks_t;
 typedef util::singleton<stacks_t> stacks_singleton;
 
+// This has to be a singleton what we put in it (stacks) are singletons
 inline stacks_t& stacks() { return stacks_singleton::instance(); }
 
 } // namespace vpush
