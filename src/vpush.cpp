fe5ed247e70b4c99487797d3a4b7e36f8ee3a1c6
Vivek Sekhar <vivek@xmain.com>
2011-01-21 01:55:39 -0500

    Updated testing code

--- src/vpush.cpp
+++ src/vpush.cpp
@@ -8,21 +8,31 @@
 
 struct badtype {
 	badtype(int) {}
-private:
+//private:
 	badtype();
 };
 
 int my_func(int i, int j) { return i+j; }
 badtype badfunc() { return badtype(1); }
+int add_1(int i) { return i+1; }
 
 int main(int argc, char** argv) {
 	using vpush::stack;
 	using vpush::stacks;
-	vpush::register_(my_func, "my_func");
-	vpush::register_(badfunc, "bad_func");
+	using vpush::register_;
+	using vpush::codes;
+	using vpush::detail::type_checker;
+	
+	register_(my_func, "my_func");
+	//register_(badfunc, "bad_func");
+	register_(add_1, "add_1");
 	std::cout << "Stacks count: " << stacks().size() << std::endl;
 	stack<int>().push(5);
 	std::cout << "Int stack: " << stack<int>().pop() << std::endl;
+	stack<int>().push(5);
+	codes["add_1"]->check();
+	codes["add_1"]->exec();
+	std::cout << "add_1: " << stack<int>().pop() << std::endl;
 	return 0;
 }
 
