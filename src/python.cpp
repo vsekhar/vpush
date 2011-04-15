240a552d6dca53e0f0d3b219888330181ed280db
Vivek Sekhar <vivek@xmain.com>
2011-04-14 20:58:43 -0400

    More python stuff

--- src/python.cpp
+++ src/python.cpp
@@ -1,6 +1,8 @@
 #include <string>
 
 #include <boost/python.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
 
 #include <vpush/protein.hpp>
 #include <vpush/soup.hpp>
@@ -8,18 +10,59 @@
 #include <vpush/detail/code.hpp>
 #include <vpush/detail/functions.hpp>
 
+namespace vpush {
+namespace python {
+
+using std::string;
 using namespace ::boost::python;
 using namespace ::vpush;
 using namespace ::vpush::detail;
+
+Protein random_protein(std::size_t s) {
+	Protein ret;
+	for(std::size_t i = 0; i < s; ++i)
+		push<Exec>(ret, functions.get_random());
+	return ret;
+}
+
+template <typename T>
+void do_push_code(Protein &p, string name) {
+	push<T>(p, functions.get_code(name));
+}
+
+template <typename T>
+void do_push_code_close(Protein &p) {
+	push<T>(p, T::CLOSE);
+}
+
+template <typename T>
+void do_push_code_open(Protein &p) {
+	push<T>(p, T::OPEN);
+}
+
+Exec byName(string n) { return functions.get_code(n); }
+Exec random() { return functions.get_random(); }
 
 BOOST_PYTHON_MODULE(vpush) {
 	// on import
 	library::initialize();
 	
 	// Code, Exec, and functions
-	class_<Code>("Code", no_init)
-		.def_readonly("type", &Code::type)
+	enum_<Code::codetype>("CodeType")
+		.value("OPEN", Code::OPEN)
+		.value("CLOSE", Code::CLOSE)
+		.export_values()
 		;
+
+	class_<Code>("Code", no_init)
+		.def(init<Code::codetype>(args("type")))
+		.def_readonly("type", &Code::type)
+		.def("byName", byName)
+		.staticmethod("byName")
+		.def("random", random)
+		.staticmethod("random")
+		;
+
 	class_<Exec, bases<Code> >("Exec", no_init);
 	
 	class_<functions_t>("Functions", no_init)
@@ -29,12 +72,28 @@ BOOST_PYTHON_MODULE(vpush) {
 	scope().attr("functions") = functions;
 	
 	// Protein and Soup
-	class_<Protein>("Protein");
-	def("print_protein", print_trace);
+	class_<Protein>("Protein")
+		.def_readwrite("energy", &Protein::energy)
+		.def("random", random_protein)
+		.staticmethod("random")
+		;
+
+	def("random_protein", random_protein);
+	def("push_code", do_push_code<Code>);
+	def("push_code_open", do_push_code_open<Code>);
+	def("push_code_close", do_push_code_close<Code>);
+	def("push_exec", do_push_code<Exec>);
+	def("push_exec_open", do_push_code_open<Exec>);
+	def("push_exec_close", do_push_code_close<Exec>);
+
 	class_<soup_t>("Soup")
 		.def("__len__", &soup_t::size)
 		.def("set_size", &soup_t::set_size)
 		.def("push_back", &soup_t::push_back)
 		;
+	scope().attr("soup") = soup;
 }
+
+} // namespace python
+} // namespace vpush
 
