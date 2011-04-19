274a166af4aebd8a03433fc83ad99107a40777cf
Vivek Sekhar <vivek@xmain.com>
2011-04-18 23:30:01 -0400

    typo

--- src/detail/code.cpp
+++ src/detail/code.cpp
@@ -5,7 +5,7 @@ namespace vpush {
 namespace detail {
 
 std::ostream& operator<<(std::ostream& o, const Code& c) {
-	select(c.type) {
+	switch(c.type) {
 		case Code::OPEN:	o << "("; break;
 		case Code::CLOSE:	o << ")"; break;
 		default:			o << vpush::functions.get_name(c.fptr); break;
