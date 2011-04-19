dd92ac4b972062f28373d7c65d5adfb228e2d387
Vivek Sekhar <vivek@xmain.com>
2011-04-18 23:31:13 -0400

    Tests should run with Debug config

--- tests/vpush.py
+++ tests/vpush.py
@@ -1,4 +1,4 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Release'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Debug'))
 from vpush import *
