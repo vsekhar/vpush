2c80b3ee90cc25b6332499d755e7d114ca6abf2b
Vivek Sekhar <vivek@xmain.com>
2011-04-21 00:52:16 -0400

    Grind testing and exit codes

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import traceback
+from sys import exit
 from tests import *
 
 def test_list():
@@ -14,10 +16,16 @@ def test_list():
 def main():
 	tests = test_list()
 	for test in tests:
-		if not test():
-			print("Test failed: %s" % test)
+		try:
+			if not test():
+				print("Test failed: %s" % test)
+				exit(1)
+		except:
+			traceback.print_exc()
 			exit(1)
+
 	print("All passed")
+	exit(0)
 
 if __name__ == '__main__':
 	main()
