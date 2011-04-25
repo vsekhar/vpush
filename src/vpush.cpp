bc6b9bd63713bcc912e8d3eb6dc535a2644c1ac5
Vivek Sekhar <vivek@xmain.com>
2011-04-25 16:03:31 -0400

    Killed main() test code (moved to python test code)

--- src/vpush.cpp
+++ src/vpush.cpp
@@ -1,29 +1,4 @@
-#include <iostream>
-#include <fstream>
-
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
-
 #include <vpush/vpush.hpp>
-
-using std::cout;
-using std::endl;
-
-using vpush::Protein;
-using vpush::random_protein;
-using vpush::stack;
-using vpush::size;
-using vpush::clear;
-using vpush::pop;
-using vpush::push;
-using vpush::pop_second;
-using vpush::push_second;
-using vpush::detail::Code;
-using vpush::detail::Exec;
-using vpush::functions;
-using vpush::soup;
-
-namespace library = ::vpush::library;
 
 namespace vpush {
 
@@ -33,47 +8,3 @@ void initialize() {
 }
 
 } // namespace vpush
-
-int main(int argc, char** argv) {
-	library::initialize();
-
-	Protein p = random_protein(1000);
-	p.energy = 2000;
-	push<int>(p, 7);
-	push<int>(p, 31);
-	push<int>(p, 4);
-	push<int>(p, 8);
-
-	push<Code>(p, Code(Code::CLOSE));
-	push<Code>(p, functions.get_code("RANDOM.CODE"));
-	push<Code>(p, Code(Code::CLOSE));
-	push<Code>(p, functions.get_code("MAKELIST.CODE"));
-	push<Code>(p, Code(Code::OPEN));
-	push<Code>(p, Code(Code::OPEN));
-	push<Exec>(p, functions.get_code("QUOTE.EXEC"));
-	push<Exec>(p, functions.get_code("DUP.CODE"));
-
-	soup.push_back(p);
-	{
-		std::ofstream out("tmp");
-		boost::archive::text_oarchive ar(out);
-		ar & p;
-		ar & soup;
-	}
-
-	Protein p2;
-	{
-		std::ifstream in("tmp");
-		boost::archive::text_iarchive ar(in);
-		ar & p2;
-		ar & soup;
-	}
-	
-	for(unsigned int i = 0; i < 100; ++i) {
-		Protein run(p2);
-		vpush::run_protein(run);
-	}
-	
-	return 0;
-}
-
