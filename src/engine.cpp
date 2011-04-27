27d0a4234f0e46b4e91436db6b8f3749d51141ed
Vivek Sekhar <vivek@xmain.com>
2011-04-27 16:42:51 -0400

    Factoring out op-code step for future re-entrant runs

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#ifdef _DEBUG
 #include <boost/assert.hpp>
-#endif
 
 #include <vpush/engine.hpp>
 #include <vpush/stackops.hpp>
@@ -12,37 +10,44 @@
 
 namespace vpush {
 
+double step(Protein& p, bool trace) {
+	using detail::Exec;
+	if(top<Exec>(p).type == Exec::OPEN) {
+		detail::unwind(stack<Exec>(p));
+		return 0;
+	}
+	else if(top<Exec>(p).type == Exec::CLOSE)
+		throw detail::unmatched_brackets();
+	else {
+		Exec e = pop<Exec>(p);
+		if(functions.get_types(e.fptr).check(p)) {
+			try {
+				double cost = e.fptr(p);
+				// for Boost 1.46
+				// BOOST_ASSERT_MSG(cost >= 0, "Function returned negative cost: " + functions.get_name(e));
+				BOOST_ASSERT(cost >= 0);
+				p.energy -= cost;
+				if(cost > 0 && trace)
+					std::cout << "Energy after " << e << ": " << p.energy << std::endl;
+				return cost;
+			}
+			catch(const detail::stack_underflow &exc) {
+				// Append op-code
+				throw detail::stack_underflow(exc, std::string(" ")+functions.get_name(e));
+			}
+		}
+		return 0;
+	}
+}
+
+// NB: have to use double& to output cost because multi_index_container takes
+// functor by value...
 void engine(Protein& p, double& total_cost, bool trace) {
 	using detail::Exec;
 	p.running = true;
-	while(!empty<Exec>(p) && p.energy > 0 && p.running) {
-		if(top<Exec>(p).type == Exec::OPEN)
-			detail::unwind(stack<Exec>(p));
-		else if(top<Exec>(p).type == Exec::CLOSE)
-			throw detail::unmatched_brackets();
-		else {
-			Exec e = pop<Exec>(p);
-			if(!e.fptr)
-				throw std::runtime_error("Null fptr");
-			if(functions.get_types(e.fptr).check(p)) {
-				try {
-					double cost = e.fptr(p);
-#ifdef _DEBUG
-					BOOST_ASSERT(cost >= 0);
-#endif
-					p.energy -= cost;
-					total_cost += cost;
-					if(cost > 0 && trace)
-						std::cout << "Energy after " << e << ": " << p.energy << std::endl;
-				}
-				catch(const detail::stack_underflow &exc) {
-					// Append op-code
-					throw detail::stack_underflow(exc, std::string(" ")+functions.get_name(e));
-				}
-			}
-		}
-	}
-	detach_gestator();
+	clear_gestator();
+	while(!empty<Exec>(p) && p.energy >= 1 && p.running)
+		total_cost += step(p, trace);
 	//fitness testing and energy rewards??
 }
 
